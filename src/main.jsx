1c000989720083f9f448b5194509990b97c00f72
Raúl Oyarzún Becerra <r.oyarzun83@gmail.com>
2023-07-30 19:37:33 -0400

    Update Main

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,10 @@ import { BrowserRouter } from 'react-router-dom'
 
 import './index.css'
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+
 )
