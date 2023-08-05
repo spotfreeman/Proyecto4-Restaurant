9d928c914d0dd1d8e953fda7520623539ecc3d44
Raúl Oyarzún Becerra <r.oyarzun83@gmail.com>
2023-08-05 07:08:34 -0400

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 
@@ -10,7 +9,7 @@ export const Footer = () => {
             <Card>
                 <Card.Header>Footer</Card.Header>
                 <Card.Body>
-                    <Card.Title>Footer Titulo</Card.Title>
+                    <Card.Title className=''>Footer Titulo</Card.Title>
                     <Card.Text>
                         With supporting text below as a natural lead-in to additional content.
                     </Card.Text>
