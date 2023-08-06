40f4d4b136f54ba83052b6adbbc05992b630bbb4
Raul <r.oyarzun83@gmail.com>
2023-08-05 22:55:31 -0400

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,11 @@
 export const About = () => {
     return (
+        <>
         <div>
             <h1>About</h1>
+            <h3>Ubicacion en Maps</h3>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1664.385321227854!2d-70.5928757063461!3d-33.45528155776547!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2scl!4v1691283636778!5m2!1ses-419!2scl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
+        </>
     );
 };
