6f929d18616893108fafc096c859dd4e87655d29
Raul <r.oyarzun83@gmail.com>
2023-08-09 22:51:08 -0400

    updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,21 +12,22 @@ export const Home = () => {
         <div>
             <CarouselApp />
         </div>
+
+        <Card className="m-2">
         <div>
-            <h1 className="text-center">Especialidades</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque sapiente atque quis eos necessitatibus commodi doloribus impedit modi ipsum nemo, nostrum, placeat nihil optio dolorem. Consequatur voluptatibus illo quas alias.</p>
-            <h3>Subtitulo</h3>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis fugit architecto, minus eaque necessitatibus ipsa porro ducimus totam illum atque itaque quod, vitae deserunt. Unde dolorum aliquid quaerat sit quisquam.</p>
+            <h1 className="text-center m-2">Especialidades</h1>
+            <p className="p-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque sapiente atque quis eos necessitatibus commodi doloribus impedit modi ipsum nemo, nostrum, placeat nihil optio dolorem. Consequatur voluptatibus illo quas alias.</p>
+            <h3 className="text-center m-2">Subtitulo</h3>
+            <p className="p-2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis fugit architecto, minus eaque necessitatibus ipsa porro ducimus totam illum atque itaque quod, vitae deserunt. Unde dolorum aliquid quaerat sit quisquam.</p>
         </div>
+        </Card>
 
         <Container>
             <Row>
                 <Col>
                 <Card style={{ width: 'auto' }}>
                   <Card.Body>
-                    
                       <Image src={imagenVertical} />
-
                   </Card.Body>
                 </Card>
                 </Col>
@@ -48,11 +49,7 @@ export const Home = () => {
                 </Col>
             </Row>
 
-            <Row>
-                <Col>Columna 1</Col>
-                <Col>Columna 2</Col>
-                <Col>Columna 3</Col>
-            </Row>
+
         </Container>
         </>
     );
