847d04664257ce65b57fc7b96bf2fb2763a6361a
Raul <r.oyarzun83@gmail.com>
2023-08-26 00:27:49 -0400

    Mejoras

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -116,21 +116,6 @@ function BasicExample() {
                     </Row>
                 </Accordion.Body>
             </Accordion.Item>
-
-            <Accordion.Item eventKey="3">
-                <Accordion.Header>Postres</Accordion.Header>
-                <Accordion.Body>
-                    <Image src={imagen01} />
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip ex ea commodo consequat. Duis aute irure dolor in
-                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum.
-                </Accordion.Body>
-            </Accordion.Item>
-
         </Accordion>
     );
 }
--- src/components/CarouselApp.jsx
+++ src/components/CarouselApp.jsx
@@ -11,26 +11,24 @@ function CarouselApp() {
       <Carousel.Item>
         <Image src={imagen01} style={{ width: '100%' }} />
         <Carousel.Caption>
-          <h3>Ejemplo 01</h3>
-          <p>Ideas para comer</p>
+          <h3>Las mejores pizzas de la comuna</h3>
+
         </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item>
         <Image src={imagen02} style={{ width: '100%' }} />
         <Carousel.Caption>
-          <h3>Ejemplo 02</h3>
-          <p>Test .</p>
+          <h3>Gran variedad de ingredientes</h3>
+
         </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item>
         <Image src={imagen03} style={{ width: '100%' }} />
         <Carousel.Caption>
-          <h3>Ejemplo 03</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          <h3>Jugos naturales de todos los sabores</h3>
+
         </Carousel.Caption>
       </Carousel.Item>
 
